3692eeedd5569bc5c97ab21ab44b375659a79795
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 00:42:52 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // write code here
+const array = [
+  { id: 1, name: 'Pepsi' },
+  { id: 2, name: 'Mirinda' },
+  { id: 3, name: 'Coke' },
+  { id: 4, name: 'Fanta' },
+  { id: 5, name: 'Sprite' }
+];
+const result = array.filter(item => item.id !== 4);
+console.log(result);
+
